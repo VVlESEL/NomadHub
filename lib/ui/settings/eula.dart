9b0e3deb0746bf6f817ac761bebcb11ed6db7566
Rene Balke <renebalke2@gmail.com>
2018-08-06 17:58:49 +0200

    added eula

---
+++ lib/ui/settings/eula.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+class PrivacyPolicyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Privacy Policy"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Text(
+            "Nomad Hub 1.0, Copyright (c) 2018 BM Trading GmbH",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text(
+            "*** END USER LICENSE AGREEMENT ***",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text(
+            "IMPORTANT: PLEASE READ THIS LICENSE CAREFULLY BEFORE USING THIS SOFTWARE.",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text(
+            "1. LICENSE",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("By receiving, opening the file package, and/or using Nomad Hub 1.0 ('Software') containing this software, you agree that this End User User License Agreement(EULA) is a legally binding and valid contract and agree to be bound by it. You agree to abide by the intellectual property laws and all of the terms and conditions of this Agreement."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("Unless you have a different license agreement signed by BM Trading GmbH your use of Nomad Hub 1.0 indicates your acceptance of this license agreement and warranty."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("If you do not agree to be bound by this agreement, remove Nomad Hub 1.0 from your mobile."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text(
+            "2. USER AGREEMENT",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text(
+            "2.2 Use Restrictions",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("You shall use Nomad Hub 1.0 in compliance with all applicable laws and not for any unlawful purpose. Without limiting the foregoing, use, display or distribution of Nomad Hub 1.0 together with material that is pornographic, racist, vulgar, obscene, defamatory, libelous, abusive, promoting hatred, discriminating or displaying prejudice based on religion, ethnic heritage, race, sexual orientation or age is strictly prohibited."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("If any person other than yourself uses Nomad Hub 1.0 registered in your name, regardless of whether it is at the same time or different times, then this agreement is being violated and you are responsible for that violation!"),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("2.3 Copyright Restriction", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("This Software contains copyrighted material, trade secrets and other proprietary material. You shall not, and shall not attempt to, modify, reverse engineer, disassemble or decompile Nomad Hub 1.0. Nor can you create any derivative works or other works that are based upon or derived from Nomad Hub 1.0 in whole or in part."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("BM Trading GmbH's name, logo and graphics file that represents Nomad Hub 1.0 shall not be used in any way to promote products developed with Nomad Hub 1.0 . BM Trading GmbH retains sole and exclusive ownership of all right, title and interest in and to Nomad Hub 1.0 and all Intellectual Property rights relating thereto."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("Copyright law and international copyright treaty provisions protect all parts of Nomad Hub 1.0, products and services. No program, code, part, image, audio sample, or text may be copied or used in any way by the user except as intended within the bounds of the single user program. All rights not expressly granted hereunder are reserved for BM Trading GmbH."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("2.4 Limitation of Responsibility", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("You will indemnify, hold harmless, and defend BM Trading GmbH , its employees, agents and distributors against any and all claims, proceedings, demand and costs resulting from or in any way connected with your use of BM Trading GmbH's Software."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("In no event (including, without limitation, in the event of negligence) will BM Trading GmbH , its employees, agents or distributors be liable for any consequential, incidental, indirect, special or punitive damages whatsoever (including, without limitation, damages for loss of profits, loss of use, business interruption, loss of information or data, or pecuniary loss), in connection with or arising out of or related to this Agreement, Nomad Hub 1.0 or the use or inability to use Nomad Hub 1.0 or the furnishing, performance or use of any other matters hereunder whether based upon contract, tort or any other theory including negligence."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("2.5 Warranties", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("Except as expressly stated in writing, BM Trading GmbH makes no representation or warranties in respect of this Software and expressly excludes all other warranties, expressed or implied, oral or written, including, without limitation, any implied warranties of merchantable quality or fitness for a particular purpose."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("2.6 Governing Law", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("This Agreement shall be governed by the law of the Germany applicable therein. You hereby irrevocably attorn and submit to the non-exclusive jurisdiction of the courts of Germany therefrom. If any provision shall be considered unlawful, void or otherwise unenforceable, then that provision shall be deemed severable from this License and not affect the validity and enforceability of any other provisions."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("2.7 Termination", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("Any failure to comply with the terms and conditions of this Agreement will result in automatic and immediate termination of this license. Upon termination of this license granted herein for any reason, you agree to immediately cease use of Nomad Hub 1.0 and destroy all copies of Nomad Hub 1.0 supplied under this Agreement. The financial obligations incurred by you shall survive the expiration or termination of this license."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("3. DISCLAIMER OF WARRANTY", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("THIS SOFTWARE AND THE ACCOMPANYING FILES ARE SOLD 'AS IS' AND WITHOUT WARRANTIES AS TO PERFORMANCE OR MERCHANTABILITY OR ANY OTHER WARRANTIES WHETHER EXPRESSED OR IMPLIED. THIS DISCLAIMER CONCERNS ALL FILES GENERATED AND EDITED BY Nomad Hub 1.0 AS WELL."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("4. CONSENT OF USE OF DATA", style: TextStyle(fontSize: 20.0),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+          Text("You agree that BM Trading GmbH may collect and use information gathered in any manner as part of the product support services provided to you, if any, related to Nomad Hub 1.0.BM Trading GmbH may also use this information to provide notices to you which may be of use or interest to you."),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
